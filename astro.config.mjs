892989ee85a7ba4846901768e4ca49a66173961c
hex46 <fa+git@hex46.fr>
2024-03-04 21:34:28 +0100

    :wrench: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
-
 import rehypeExternalLinks from "rehype-external-links";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://hex46.fr",
@@ -8,8 +8,12 @@ export default defineConfig({
     rehypePlugins: [
       [
         rehypeExternalLinks,
-        { rel: ["noreferrer", "external"], target: "_blank" },
+        {
+          rel: ["noreferrer", "external"],
+          target: "_blank",
+        },
       ],
     ],
   },
+  integrations: [sitemap()],
 });
